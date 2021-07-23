d790ad2047849cd41a892b3b18ef8b8e8048843b
AMmetro <metro.trend.now@gmail.com>
2021-07-23 20:55:24 +0300

    Andrei test Commit

--- src/n1-main/App.tsx
+++ src/n1-main/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Routes from './m1-ui/Routes';
 import Header from "./m1-ui/Header/Header";
+import AndreiComponent from "./andreiDir/AndreiComponent";
 
 function App() {
     console.log('Render App' )
   return (
     <div className="App">
+        <AndreiComponent/>
         <Header/>
       <Routes/>
     </div>
---
+++ src/n1-main/andreiDir/AndreiComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function AndreiComponent() {
+
+  return (
+    <div>
+   <h1>Work with git - Test text from new branch</h1>
+    </div>
+  );
+}
+
+export default AndreiComponent;
