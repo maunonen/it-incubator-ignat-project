7ab0e1c3600f653374c72765eee09972670a8221
AMmetro <metro.trend.now@gmail.com>
2021-07-23 21:46:14 +0300

    Andrei 3 change

--- src/n1-main/App.tsx
+++ src/n1-main/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Routes from './m1-ui/Routes';
 import Header from "./m1-ui/Header/Header";
-import AndreiComponent from "./andreiDir/AndreiComponent";
+
 
 function App() {
     console.log('Render App' )
@@ -10,7 +10,6 @@ function App() {
     <div className="App">
       <Header/>
       <Routes/>
-      <AndreiComponent/>
     </div>
   );
 }
