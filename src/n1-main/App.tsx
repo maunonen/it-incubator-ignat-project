fd05e34bdc32c311ba65f615f138b98cf0903fad
RamanSlinka <romuch1986@gmail.com>
2021-07-23 19:48:35 +0300

    first commit )

--- src/n1-main/App.tsx
+++ src/n1-main/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import Routes from './m1-ui/Routes';
 import Header from "./m1-ui/Header/Header";
 
+// my first commit
+
 function App() {
     console.log('Render App' )
   return (
