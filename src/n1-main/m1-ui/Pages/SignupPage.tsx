c6368b6aa0853267a4799126e7eb6a35943cd357
RamanSlinka <romuch1986@gmail.com>
2021-07-24 21:06:05 +0300

    add SignupPage

--- src/n1-main/m1-ui/Pages/SignupPage.tsx
+++ src/n1-main/m1-ui/Pages/SignupPage.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import {FormControl, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
 
-const SignupPage : React.FC = () => {
+const SignupPage: React.FC = () => {
     return (
-        <>
-            <h1>Sign up</h1>
-        </>
+
+        <Grid container justify="center">
+
+
+            <Grid item xs={4}>
+                <FormControl>
+                    <FormLabel>
+                        <h1>IT incubator</h1>
+                        <p>Sign up </p>
+
+                    </FormLabel>
+                    <FormGroup>
+                        <TextField
+                            label="Email"
+                            margin="normal"
+                        />
+                        <TextField
+                            type="password"
+                            label="Password"
+                            margin="normal"
+                        />
+                        <TextField
+                            type="password"
+                            label="Password"
+                            margin="normal"
+                        />
+
+                        <FormGroup>
+                            <Button type={'reset'} variant={'contained'} color={'primary'}>Cancel</Button>
+                            <Button type={'submit'} variant={'contained'} color={'primary'}>Register</Button>
+                        </FormGroup>
+                    </FormGroup>
+                </FormControl>
+            </Grid>
+        </Grid>
+
+
     )
 }
 export default SignupPage
