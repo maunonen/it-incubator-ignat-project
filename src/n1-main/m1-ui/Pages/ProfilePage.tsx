9aba1c19873867e12a23d0d4ddd05c601e804bbd
AMmetro <metro.trend.now@gmail.com>
2021-07-29 19:58:11 +0300

    developed profile component

--- src/n1-main/m1-ui/Pages/ProfilePage.tsx
+++ src/n1-main/m1-ui/Pages/ProfilePage.tsx
@@ -1,22 +1,61 @@
-import React from 'react'
-import {useSelector} from "react-redux";
+
+import React, {useState} from 'react'
+import {Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
+import {useDispatch, useSelector} from "react-redux";
+import {loggedInTC} from "../../m2-bll/redux/auth-reducer"
 import {AppStoreType} from "../../m2-bll/redux/store";
+import {Redirect} from "react-router-dom";
+import {PATH} from "../Routes";
 
-const ProfilePage : React.FC = () => {
 
+
+export const ProfilePage: React.FC = () => {
+
+    const dispatch = useDispatch()
+    const isLoggedIn = useSelector<AppStoreType, any>(state => state.auth)
     const userProfile = useSelector<AppStoreType, any>(state => state.profile)
 
-    // commit
 
-    return (
-        <>
-            <h1>Profile</h1>
-            <h3>Id {userProfile._id}</h3>
-            <h3>Email {userProfile.email}</h3>
-            <h3>Name {userProfile.name}</h3>
-            <h3>Avatar <img src={userProfile.avatar} style={{width: 100}}/> </h3>
-            <h3>PublicCardPacksCount= {userProfile.publicCardPacksCount}</h3>
-        </>
-    )
+
+    if (isLoggedIn.isLoggedIn) {
+        return (
+            <Grid container justify="center">
+
+                <Grid item xs={4}>
+
+
+                    <h1>User profile</h1>
+                    <h3>Id {userProfile._id}</h3>
+                    <h3>Email {userProfile.email}</h3>
+                    <h3>Name {userProfile.name}</h3>
+                    <h3>Avatar <img src={userProfile.avatar} style={{width: 100}}/> </h3>
+                    <h3>PublicCardPacksCount= {userProfile.publicCardPacksCount}</h3>
+
+
+
+                    <FormControl>
+                        <FormLabel>
+                            Login
+                            <br/>
+                        </FormLabel>
+                        <TextField
+                            label="Email"
+                            margin="normal"
+                            name="Email"
+                            value={"email"}
+                        />
+                    </FormControl>
+
+                </Grid>
+            </Grid>
+        )
+    } else {
+        return  (
+            <Redirect from={PATH.ALL_ROUTES} to="/login"/>
+            )
+    }
 }
+
 export default ProfilePage
+
+
